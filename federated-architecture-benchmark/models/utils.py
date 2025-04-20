70bd25467c2c56969844da62c15ec236626cba7d
Tanveer1100 <shaikhtanveer510@gmail.com>
2025-04-19 20:06:54 -0700

    Update utils.py

--- federated-architecture-benchmark/models/utils.py
+++ federated-architecture-benchmark/models/utils.py
@@ -8,5 +8,8 @@ def get_model(model_type, **kwargs):
     elif model_type == "rnn":
         from models.rnn import RNN
         return RNN(**kwargs)
+    elif model_type == "lstm":
+        from models.rnn import LSTMClassifier
+        return LSTMClassifier(**kwargs)
     else:
         raise ValueError(f"Unknown model type: {model_type}")
